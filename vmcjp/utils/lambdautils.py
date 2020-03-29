df4378bc0baf479669ecb7b82c7ec5a41f429cd9
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-29 16:22:33 +0900

    Update lambdautils.py

--- vmcjp/utils/lambdautils.py
+++ vmcjp/utils/lambdautils.py
@@ -1,4 +1,5 @@
 import json
+import boto3
 
 from botocore.session import Session
 from botocore.config import Config
@@ -28,15 +29,21 @@ def call_lambda_async(function, data):
     )
 
 def call_lambda_sync(function, data):
-    s = Session()
-    clientLambda = s.create_client(
-        "lambda", 
-        config=Config(retries={'max_attempts': 0})
-    )
-    response = clientLambda.invoke(
+#    s = Session()
+#    clientLambda = s.create_client(
+#        "lambda", 
+#        config=Config(retries={'max_attempts': 0})
+#    )
+    lambda = boto3.client('lambda')
+    response = client.invoke(
         FunctionName=function,
         InvocationType="RequestResponse",
         Payload=json.dumps(data)
     )
+#    response = clientLambda.invoke(
+#        FunctionName=function,
+#        InvocationType="RequestResponse",
+#        Payload=json.dumps(data)
+#    )
     body = json.loads(response['Payload'].read())
     return body

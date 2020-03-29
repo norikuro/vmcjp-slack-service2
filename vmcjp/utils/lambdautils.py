c1ba5512cc3f4e5fb086bfb18b2144e483579e5b
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-29 18:09:12 +0900

    Update lambdautils.py

--- vmcjp/utils/lambdautils.py
+++ vmcjp/utils/lambdautils.py
@@ -2,23 +2,23 @@ import json
 import boto3
 #import logging
 
-from botocore.session import Session
+#from botocore.session import Session
 from botocore.config import Config
 
 #logger = logging.getLogger()
 #logger.setLevel(logging.INFO)
 
-def call_lambda(function, data):
-    s = Session()
-    clientLambda = s.create_client(
-        "lambda", 
-        config=Config(retries={'max_attempts': 0})
-    )
-    clientLambda.invoke(
-        FunctionName=function,
-        InvocationType="Event",
-        Payload=json.dumps(data)
-    )
+#def call_lambda(function, data):
+#    s = Session()
+#    clientLambda = s.create_client(
+#        "lambda", 
+#        config=Config(retries={'max_attempts': 0})
+#    )
+#    clientLambda.invoke(
+#        FunctionName=function,
+#        InvocationType="Event",
+#        Payload=json.dumps(data)
+#    )
 
 def call_lambda_async(function, data):
     config = Config(retries={'max_attempts': 0})

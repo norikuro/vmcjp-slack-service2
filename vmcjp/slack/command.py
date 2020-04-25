47de1486c9a7b2e8f3f114152784024eed809113
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-04-25 16:48:14 +0900

    Update command.py

--- vmcjp/slack/command.py
+++ vmcjp/slack/command.py
@@ -129,7 +129,7 @@ def create_sddc(event):
         "slack_vmc", data
     )
 #    max_hosts = 10 #for test
-    max_hosts = 1 #for test
+    max_hosts = 5 #for test
     event.update({"max_hosts": max_hosts})
     if max_hosts < 1:
         message_handler(msg_const.NOT_ENOUGH, event)
